73e7c1ff506572da307c02381421930f724b79f2
Bedi Samuel Bubune <bedibubune888@gmail.com>
2025-06-25 16:43:52 +0000

    new

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 // Configuration
 const config = {
-  databaseId: 'career4me',
+  databaseId: 'career4m',
   jobsCollectionId: 'jobs',
   talentsCollectionId: 'talents',
   employersCollectionId: '67d870d800046e4c2a61',
